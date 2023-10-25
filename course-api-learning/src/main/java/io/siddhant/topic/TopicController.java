2b5e71bde97b165dae1fbe811125b402fc61ff57
Siddhant Jaiswal <siddhantjaiswal363@gmail.com>
2023-10-25 23:50:34 +0530

    Topic API

--- course-api-learning/src/main/java/io/siddhant/topic/TopicController.java
+++ course-api-learning/src/main/java/io/siddhant/topic/TopicController.java
@@ -1,11 +1,18 @@
 package io.siddhant.topic;
 import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
 	
+	@Autowired
+	private TopicService topicService;
 	
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics()
@@ -16,7 +23,21 @@ public class TopicController {
 		topics.add(new Topic("124","mithun", "infa"));
 		topics.add(new Topic("125","rajan", "infa"));
 		
-		return topics;
+		return topicService.getAllTopics();
 	}
+	
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id)
+	{
+		return	topicService.getTopic(id);
+	}
+	@RequestMapping(method= RequestMethod.POST, value = "/topics")
+	public void addTopic(@RequestBody Topic topic)
+	{
+		topicService.addTopic(topic);
+		return;
+		
+	}
+	
 
 }
