0b7ee35cda890bedcb79ac6c43ea76e5a3ba54b5
Siddhant Jaiswal <siddhantjaiswal363@gmail.com>
2023-10-26 00:43:19 +0530

    update

--- course-api-data/src/main/java/io/siddhant/topic/TopicService.java
+++ course-api-data/src/main/java/io/siddhant/topic/TopicService.java
@@ -2,6 +2,7 @@ package io.siddhant.topic;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -30,7 +31,8 @@ private List<Topic> topics = new ArrayList<>();
 	
 	public Topic getTopic(String id)
 	{
-	return	topics.stream().filter(t-> t.getId().equals(id)).findFirst().get();
+		Optional<Topic> t = topicRepository.findById(id);
+	return	t.orElse(null);
 	
 	}
 	public void addTopic(Topic topic)
@@ -40,21 +42,12 @@ private List<Topic> topics = new ArrayList<>();
 	}
 	public void updateTopic(String id, Topic topic)
 	{
-		for (int i = 0; i<topics.size(); i++)
-		{
-			Topic t = topics.get(i);
-			if(t.getId().equals(i))
-			{
-				topics.set(i, topic);
-				return;
-			}
-		}
+		topicRepository.save(topic);
 	}
 	
 	public void deleteTopic(String id)
 	{
-		topics.removeIf(t -> t.getId().equals(id));
-		
+		topicRepository.deleteById(id);
 	}
 }
 
